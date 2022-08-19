24a3edcf05eadce6bd83b72d40b9730945806720
JE <prm1247@naver.com>
2022-08-19 16:20:40 +0900

    COMMIT

---
+++ src/main/java/dao/StoreInfoDAOTest.java
@@ -0,0 +1,38 @@
+package dao;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import vo.StoreInfoVO;
+
+class StoreInfoDAOTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void test() {
+		StoreInfoDAO dao = new StoreInfoDAO();
+		StoreInfoVO vo =dao.selectOne(1);
+		assertEquals("더 파크뷰", vo.getStoreName());
+	}
+
+}
